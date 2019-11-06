0e5a81a967acdbd5a892b6233bad42cdc61d480f
tarokkk <gubasanyi@gmail.com>
2019-11-06 20:44:11 +0100

    add event/s flag and prometheus metrics

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module github.com/banzaicloud/nginx-log-generator
+module github.com/banzaicloud/log-generator
 
 go 1.13
 
 require (
-	github.com/Pallinder/go-randomdata v1.2.0 // indirect
-	github.com/mroth/weightedrand v0.2.1 // indirect
+	github.com/Pallinder/go-randomdata v1.2.0
+	github.com/mroth/weightedrand v0.2.1
+	github.com/prometheus/client_golang v1.2.1
+	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/Pallinder/go-randomdata"
 	wr "github.com/mroth/weightedrand"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const NginxTimeFormat = "02/Jan/2006:15:04:05 -0700"
 
-// (?<remote>[^ ]*) (?<host>[^ ]*) (?<user>[^ ]*) \[(?<time>[^\]]*)\] "(?<method>\S+)(?: +(?<path>[^\"]*?)(?: +\S*)?)?" (?<code>[^ ]*) (?<size>[^ ]*)(?: "(?<referer>[^\"]*)" "(?<agent>[^\"]*)"(?:\s+(?<http_x_forwarded_for>[^ ]+))?)?$/
-// 10.20.64.0 - - [05/Oct/2019:06:49:04 +0000] "GET / HTTP/1.1" 200 612 "-" "kube-probe/1.15" "-"
+type LogGen interface {
+	String() (string, float64)
+}
 
 type NginxLog struct {
 	Remote            string
@@ -36,11 +43,11 @@ func NewNginxLog() NginxLog {
 		User:              "-",
 		Time:              time.Time{},
 		Method:            "GET",
-		Path:              "/",
+		Path:              "/loggen",
 		Code:              200,
 		Size:              650,
 		Referer:           "-",
-		Agent:             "",
+		Agent:             "golang/generator",
 		HttpXForwardedFor: "-",
 	}
 }
@@ -69,21 +76,95 @@ func NewNginxLogRandom() NginxLog {
 	}
 }
 
-func (n NginxLog) String() string {
-	return fmt.Sprintf("%s %s %s [%s] \"%s %s HTTP/1.1\" %d %d %q %q %q", n.Remote, n.Host, n.User, n.Time.Format(NginxTimeFormat), n.Method, n.Path, n.Code, n.Size, n.Referer, n.Agent, n.HttpXForwardedFor)
+func (n NginxLog) String() (string, float64) {
+	message := fmt.Sprintf("%s %s %s [%s] \"%s %s HTTP/1.1\" %d %d %q %q %q", n.Remote, n.Host, n.User, n.Time.Format(NginxTimeFormat), n.Method, n.Path, n.Code, n.Size, n.Referer, n.Agent, n.HttpXForwardedFor)
+	return message, float64(len([]byte(message)))
 }
 
+var (
+	eventEmitted = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "event_emitted_total",
+		Help: "The total number of events",
+	})
+	eventEmittedBytes = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "event_emitted_total_bytes",
+		Help: "The total number of events",
+	})
+)
+
 func main() {
-	minIntervall := flag.Int("min-intervall", 3, "Minimum interval between log messages")
-	maxIntervall := flag.Int("max-intervall", 8, "Maximum interval between log messages")
-	count := flag.Int("count", 10000, "The amount of log message to emit.")
+	minIntervall := flag.String("min-intervall", "100ms", "Minimum interval between log messages")
+	maxIntervall := flag.String("max-intervall", "1s", "Maximum interval between log messages")
+	count := flag.Int("count", 0, "The amount of log message to emit.")
+	randomise := flag.Bool("randomise", true, "Randomise log content")
+	eventPerSec := flag.Int("event-per-sec", 10, "The amount of log message to emit/s")
+	metricsAddr := flag.String("metrics.addr", ":11000", "Metrics server listen address")
 
 	flag.Parse()
 
-	for i := 0; i < *count; i++ {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
-		time.Sleep(time.Duration(rand.Intn((*maxIntervall-*minIntervall)+*minIntervall)) * time.Second)
-		n := NewNginxLogRandom()
-		fmt.Println(n)
+	done := make(chan bool)
+
+	go func() {
+		i := 0
+		for {
+			// If we have count check counter
+			if *count > 0 && !(i < *count) {
+				break
+			}
+
+			var n LogGen
+			if *randomise {
+				n = NewNginxLogRandom()
+			} else {
+				n = NewNginxLog()
+			}
+
+			msg, size := n.String()
+			fmt.Println(msg)
+			eventEmitted.Inc()
+			eventEmittedBytes.Add(size)
+
+			var duration time.Duration
+			if *eventPerSec > 0 {
+				sleepTime := 1000.0 / *eventPerSec
+				duration = time.Duration(sleepTime) * time.Millisecond
+			} else {
+				// Randomise output between min and max
+				parsedMaxTime, err := time.ParseDuration(*maxIntervall)
+				if err != nil {
+					panic(err)
+				}
+				parsedMinTime, err := time.ParseDuration(*minIntervall)
+				if err != nil {
+					panic(err)
+				}
+				duration = time.Duration(rand.Int63n(int64((parsedMaxTime - parsedMinTime) + parsedMinTime)))
+			}
+			// Sleep before next flush
+			time.Sleep(duration)
+
+			// Increment counter
+			i++
+		}
+		done <- true
+	}()
+
+	go func() {
+		fmt.Println("metrics listen on: ", *metricsAddr)
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(*metricsAddr, nil)
+		fmt.Println("main loop started")
+	}()
+
+	select {
+	case <-done:
+		return
+	case <-interrupt:
+		fmt.Println("Recieved interrupt")
+		return
 	}
+
 }
