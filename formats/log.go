d7f8c2c86be42cdca5559568e14c543a178f452c
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2023-05-10 15:15:31 +0200

    chore: license fix
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>

--- formats/log.go
+++ formats/log.go
@@ -1,4 +1,16 @@
-// Copyright (c) 2023 Cisco All Rights Reserved.
+// Copyright © 2023 Kube logging authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License."""
 
 package formats
 
@@ -8,10 +20,11 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/prometheus/client_golang/prometheus"
+
 	"github.com/kube-logging/log-generator/formats/golang"
 	"github.com/kube-logging/log-generator/formats/syslog"
 	"github.com/kube-logging/log-generator/formats/web"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 // TODO: factory
