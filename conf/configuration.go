885cab5a2e5d7a2462490ed76b0f859081654927
László Várady <laszlo.varady@axoflow.com>
2023-09-26 10:33:00 +0200

    conf: make host and appname configurable
    Signed-off-by: László Várady <laszlo.varady@axoflow.com>

--- conf/configuration.go
+++ conf/configuration.go
@@ -45,6 +45,8 @@ func Init() {
 	viper.SetDefault("message.byte-per-sec", 200)
 	viper.SetDefault("message.max-random-hosts", 1000)
 	viper.SetDefault("message.max-random-apps", 100)
+	viper.SetDefault("message.host", "hostname")
+	viper.SetDefault("message.appname", "appname")
 
 	viper.SetDefault("api.addr", ":11000")
 	viper.SetDefault("api.basePath", "/")
